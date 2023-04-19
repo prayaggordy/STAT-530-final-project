f9ef89edf9a06e3c143a2e300cf5e65ff69767ed
Prayag Gordy <prayaggordy@gmail.com>
2023-04-19 03:20:48 -0500

    combine dataset

---
+++ R/lag_options.R
@@ -0,0 +1,19 @@
+remove_redistricting <- function(df,
+                                 remainder = config$data$elections$remove_remainder) {
+  df |>
+    dplyr::filter(year %% 10 != remainder)
+}
+
+simple_lag <- function(df) {
+  d <- df |>
+    remove_redistricting()
+  
+  d |>
+    dplyr::select(year, full_district, dem_margin) |>
+    dplyr::inner_join(
+      d |>
+        dplyr::mutate(next_election = year + 2) |>
+        dplyr::select(next_election, full_district, prev_dem_margin = dem_margin),
+      by = c("year" = "next_election", "full_district")
+    )
+}
