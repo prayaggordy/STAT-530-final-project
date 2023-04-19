8c5124d66d65fda289049dd64cf3b439f582e06f
Prayag Gordy <prayaggordy@gmail.com>
2023-04-19 01:37:37 -0500

    race must contain: - democrat - at least one non-democrat - only candidates with totalvotes > 1 - other filters

--- R/data_management.R
+++ R/data_management.R
@@ -3,11 +3,18 @@ read_elections <- function(fn, raw) {
 }
 
 apply_filters <- function(df,
-                          filters = config$data$elections$filters) {
+                          filters = config$data$elections$filters,
+                          min_percent = config$data$elections$min_percent,
+                          democrats = paste0("^",
+                                             config$data$elections$democrat,
+                                             "$")) {
   df |>
     dplyr::filter(dplyr::if_all(dplyr::all_of(filters), ~ !dplyr::coalesce(., FALSE)),
                   stage == "GEN",
-                  totalvotes > 1)
+                  totalvotes > 1) |>
+    dplyr::group_by(year, state, district) |>
+    dplyr::filter(any(stringr::str_detect(party, paste(democrats, collapse = "|")))) |>
+    dplyr::ungroup()
 }
 
 calculate_dem_margin <- function(df,
@@ -17,12 +24,15 @@ calculate_dem_margin <- function(df,
   df |>
     dplyr::mutate(full_district = paste(state_po, district),
                   share = candidatevotes/totalvotes,
+                  party = dplyr::coalesce(party, candidate),
                   democrat = ifelse(stringr::str_detect(party, paste(democrats, collapse = "|")),
                                     "democrat",
-                                    "highest_other") |>
-                    dplyr::coalesce("highest_other")) |>
+                                    "highest_other")) |>
     dplyr::group_by(year, full_district, democrat) |>
     dplyr::summarize(share = max(share)) |>
+    dplyr::ungroup() |>
+    dplyr::group_by(year, full_district) |>
+    dplyr::filter(dplyr::n() == 2) |>
     dplyr::ungroup() |>
     tidyr::pivot_wider(names_from = democrat, values_from = share) |>
     dplyr::mutate(dplyr::across(c(democrat, highest_other), ~ tidyr::replace_na(., 0)),
