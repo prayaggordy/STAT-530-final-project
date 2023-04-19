87904433826ba1e72bc85f6e4fbf7c8c7c2d02e2
Prayag Gordy <prayaggordy@gmail.com>
2023-04-19 01:55:44 -0500

    keep extra columns

--- R/data_management.R
+++ R/data_management.R
@@ -12,8 +12,10 @@ apply_filters <- function(df,
     dplyr::filter(dplyr::if_all(dplyr::all_of(filters), ~ !dplyr::coalesce(., FALSE)),
                   stage == "GEN",
                   totalvotes > 1) |>
+    dplyr::select(-c(dplyr::all_of(filters), stage)) |>
     dplyr::group_by(year, state, district) |>
-    dplyr::filter(any(stringr::str_detect(party, paste(democrats, collapse = "|")))) |>
+    dplyr::filter(any(stringr::str_detect(party, paste(democrats, collapse = "|"))),
+                  length(unique(totalvotes)) == 1) |>
     dplyr::ungroup()
 }
 
@@ -24,36 +26,26 @@ calculate_dem_margin <- function(df,
   df |>
     dplyr::mutate(full_district = paste(state_po, district),
                   share = candidatevotes/totalvotes,
-                  party = dplyr::coalesce(party, candidate),
                   democrat = ifelse(stringr::str_detect(party, paste(democrats, collapse = "|")),
                                     "democrat",
-                                    "highest_other")) |>
+                                    "highest_other") |>
+                    dplyr::coalesce("highest_other")) |>
     dplyr::group_by(year, full_district, democrat) |>
-    dplyr::summarize(share = max(share)) |>
+    dplyr::slice_max(share) |>
     dplyr::ungroup() |>
     dplyr::group_by(year, full_district) |>
     dplyr::filter(dplyr::n() == 2) |>
     dplyr::ungroup() |>
-    tidyr::pivot_wider(names_from = democrat, values_from = share) |>
-    dplyr::mutate(dplyr::across(c(democrat, highest_other), ~ tidyr::replace_na(., 0)),
-                  dem_margin = democrat - highest_other)
+    tidyr::pivot_wider(names_from = democrat, values_from = c(share, party, candidate, candidatevotes, writein)) |>
+    dplyr::mutate(dem_margin = share_democrat - share_highest_other)
 }
 
 get_elections <- function(fn = config$data$elections$fn,
                           raw = config$paths$raw,
                           proc = config$paths$proc) {
-  df_raw <- read_elections(fn = fn, raw = raw)
-  
-  df <- df_raw |>
+  df <- read_elections(fn = fn, raw = raw) |>
     apply_filters() |>
-    calculate_dem_margin() |>
-    dplyr::left_join(
-      df_raw |>
-        dplyr::mutate(full_district = paste(state_po, district)) |>
-        dplyr::group_by(year, full_district) |>
-        dplyr::summarize(totalvotes = max(totalvotes)),
-      by = c("year", "full_district")
-    )
+    calculate_dem_margin()
   
   readr::write_csv(df, paste0(proc, fn))
   
