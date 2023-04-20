cae81f7f682dfa0e88d204825ec24cce5d54dfa7
Prayag Gordy <prayaggordy@gmail.com>
2023-04-19 20:47:08 -0500

    bring demos into simple_lag

--- R/lag_options.R
+++ R/lag_options.R
@@ -4,7 +4,8 @@ remove_redistricting <- function(df,
     dplyr::filter(year %% 10 != remainder)
 }
 
-simple_lag <- function(df) {
+simple_lag <- function(df,
+                       demos = DF_DEMOS) {
   d <- df |>
     remove_redistricting()
   
@@ -15,5 +16,6 @@ simple_lag <- function(df) {
         dplyr::mutate(next_election = year + 2) |>
         dplyr::select(next_election, full_district, prev_dem_margin = dem_margin),
       by = c("year" = "next_election", "full_district")
-    )
+    ) |>
+    dplyr::left_join(demos, by = c("year", "full_district"))
 }
