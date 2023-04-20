6da37fd589b4a718bff9f3d01d4361547656f31d
Prayag Gordy <prayaggordy@gmail.com>
2023-04-19 23:32:42 -0500

    try different filtering scheme

--- R/lag_options.R
+++ R/lag_options.R
@@ -11,11 +11,14 @@ lu_repeated_candidate <- function(df, dem) {
 
 lag_simple <- function(df) {
   df |>
-    dplyr::select(year, full_district, dem_margin) |>
+    dplyr::select(year, full_district, dem_margin, candidate_democrat, candidate_highest_other) |>
     dplyr::inner_join(
       df |>
         dplyr::mutate(next_election = year + 2) |>
-        dplyr::select(next_election, full_district, prev_dem_margin = dem_margin),
+        dplyr::select(next_election, full_district, 
+                      prev_dem_margin = dem_margin, 
+                      prev_candidate_democrat = candidate_democrat, 
+                      prev_candidate_highest_other = candidate_highest_other),
       by = c("year" = "next_election", "full_district")
     )
 }
@@ -29,45 +32,59 @@ lag_any_candidates <- function(df,
 lag_same_dem <- function(df,
                          demos = DF_DEMOS) {
   lag_simple(df) |>
-    dplyr::inner_join(lu_repeated_candidate(df, dem = T)) |>
+    dplyr::filter(candidate_democrat == prev_candidate_democrat) |>
+    dplyr::left_join(demos, by = c("year", "full_district"))
+}
+
+lag_diff_dem <- function(df,
+                         demos = DF_DEMOS) {
+  lag_simple(df) |>
+    dplyr::filter(candidate_democrat != prev_candidate_democrat) |>
     dplyr::left_join(demos, by = c("year", "full_district"))
 }
 
 lag_same_dem_same_rep <- function(df,
                                   demos = DF_DEMOS) {
   lag_simple(df) |>
-    dplyr::inner_join(lu_repeated_candidate(df, dem = T)) |>
-    dplyr::inner_join(lu_repeated_candidate(df, dem = F)) |>
+    dplyr::filter(candidate_democrat == prev_candidate_democrat,
+                  candidate_highest_other == prev_candidate_highest_other) |>
     dplyr::left_join(demos, by = c("year", "full_district"))
 }
 
 lag_same_dem_diff_rep <- function(df,
                                   demos = DF_DEMOS) {
   lag_simple(df) |>
-    dplyr::inner_join(lu_repeated_candidate(df, dem = T)) |>
-    dplyr::anti_join(lu_repeated_candidate(df, dem = F)) |>
+    dplyr::filter(candidate_democrat == prev_candidate_democrat,
+                  candidate_highest_other != prev_candidate_highest_other) |>
     dplyr::left_join(demos, by = c("year", "full_district"))
 }
 
 lag_same_rep <- function(df,
                          demos = DF_DEMOS) {
   lag_simple(df) |>
-    dplyr::inner_join(lu_repeated_candidate(df, dem = F)) |>
+    dplyr::filter(candidate_highest_other == prev_candidate_highest_other) |>
+    dplyr::left_join(demos, by = c("year", "full_district"))
+}
+
+lag_diff_rep <- function(df,
+                         demos = DF_DEMOS) {
+  lag_simple(df) |>
+    dplyr::filter(candidate_highest_other != prev_candidate_highest_other) |>
     dplyr::left_join(demos, by = c("year", "full_district"))
 }
 
 lag_diff_dem_same_rep <- function(df,
                                   demos = DF_DEMOS) {
   lag_simple(df) |>
-    dplyr::anti_join(lu_repeated_candidate(df, dem = T)) |>
-    dplyr::inner_join(lu_repeated_candidate(df, dem = F)) |>
+    dplyr::filter(candidate_democrat != prev_candidate_democrat,
+                  candidate_highest_other == prev_candidate_highest_other) |>
     dplyr::left_join(demos, by = c("year", "full_district"))
 }
 
 lag_diff_dem_diff_rep <- function(df,
                                   demos = DF_DEMOS) {
   lag_simple(df) |>
-    dplyr::anti_join(lu_repeated_candidate(df, dem = F)) |>
-    dplyr::anti_join(lu_repeated_candidate(df, dem = F)) |>
+    dplyr::filter(candidate_democrat != prev_candidate_democrat,
+                  candidate_highest_other != prev_candidate_highest_other) |>
     dplyr::left_join(demos, by = c("year", "full_district"))
 }
